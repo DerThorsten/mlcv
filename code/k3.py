4a9073765d22ea7b98686f27518111a80aeeabd9
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2017-05-16 13:26:41 +0200

    added struct_svm slides

--- code/k3.py
+++ code/k3.py
@@ -24,27 +24,29 @@ model = dgm.models.DiscreteGraphicalModel(variable_space=variable_space)
 # add unary factors:
 
 #  phi_0(x_0) = { 0.1   if x_0  == 0
-#               { 0.9   if x_0  == 1
+#               { 0.1   if x_0  == 1
 phi_0 = dgm.value_tables.DenseValueTable(values=[0.1, 0.1])
 model.add_factor(variables=[0], value_table=phi_0)
 
-#  phi_1(x_1) = { 0.8   if x_1 == 0
-#               { 0.1   if x_1 == 1
+#  phi_1(x_1) = { 0.1   if x_1 == 0
+#               { 0.9   if x_1 == 1
 phi_1 = dgm.value_tables.DenseValueTable(values=[0.1, 0.9])
 model.add_factor(variables=[1], value_table=phi_1)
 
-#  phi_2(x_2) = { 0.1   if x_2 == 0
-#               { 0.9   if x_2 == 1
+#  phi_2(x_2) = { 0.9   if x_2 == 0
+#               { 0.1   if x_2 == 1
 phi_2 = dgm.value_tables.DenseValueTable(values=[0.9, 0.1])
 model.add_factor(variables=[2], value_table=phi_2)
 
 
 # add pairwise factors:
-# #  phi_ij(x_i, x_j) = { 0.0   if x_i == x_j
-#                       { 1.0   if x_i != x_j
+# #  phi_ij(x_i, x_j) = { 0.0    if x_i == x_j
+#                       { beta   if x_i != x_j
 beta = -1.0
 phi_ij = dgm.value_tables.DenseValueTable(values=[[0,beta],
                                                   [beta,0]])
+phi_ij = dgm.value_tables.PottsFunction(shape=[2,2], beta=beta)
+
 
 model.add_factor(variables=[0,1], value_table=phi_ij)
 model.add_factor(variables=[0,2], value_table=phi_ij)
